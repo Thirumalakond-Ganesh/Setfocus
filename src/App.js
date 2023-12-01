2d2f1a69d0b438b6c6c65e1ef42c3daa72b479b6
Thirumalakonda-Ganesh <ganitthirumala50@gmail.com>
2023-12-01 11:45:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react"
+import { useForm } from "react-hook-form"
+import './App.css'
 
-function App() {
+export default function App() {
+  const { register, handleSubmit, setFocus } = useForm()
+  const onSubmit = (data) => console.log(data)
+
+
+
+  React.useEffect(() => {
+    setFocus("firstName")
+  }, [setFocus])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h2>setFocus Example</h2>
+      <input {...register("firstName")} placeholder="First Name" />
+      <button type="submit" style={{marginLeft:"100px"}}>Submit</button>
+    </form>
+  )
 }
-
-export default App;
